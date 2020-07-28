df5d9fa2f7e0501505dcea2083e66625fa59783b
Pignataro67 <Pignataro67@gmail.com>
2020-07-28 04:47:13 -0700

    then in fetch

--- findadaycare-fe/src/actions/currentUser.js
+++ findadaycare-fe/src/actions/currentUser.js
@@ -8,11 +8,22 @@ export const setCurrentUser = user => {
 export const login = credentials => {
   return dispatch => {
     return fetch('http://localhost:3001/api/v1/login', {
+      credentials: "include",
       method: "POST",
       headers: {
       "Content-type": "application/json"
     },
-      body: JSON.stringify({email: "user@test.com", username: "test", password: "secret"})
+      body: JSON.stringify({credentials})
     })
+    .then(res => res.json())
+    .then(user => {
+      if (user.error) {
+      alert(user.error)
+    } else {
+      dispatch(setCurrentUser(user))
+        console.log("Sucess, you are logged in")
+      }
+    })
+    .catch(console.log("pass")) 
   }
 } 
