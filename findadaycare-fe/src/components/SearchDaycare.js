d2f9d3c1674e61cad7c114c2cad45649c552c3c2
Pignataro67 <Pignataro67@gmail.com>
2020-07-29 18:19:10 -0700

    adding comp.

---
+++ findadaycare-fe/src/components/SearchDaycare.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDaycareFromYelp } from '../actions/searchDaycare.js';
+
+class SearchDaycare extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello  
+      </div>
+    )   
+  }  
+} 
+
+export default SearchDaycare;
