ffe27d65536f7fd1f10e9a37efc6fcd27fe018e6
Pignataro67 <Pignataro67@gmail.com>
2020-07-31 06:53:36 -0700

    spacing

--- findadaycare-fe/src/components/ListDaycareFromYelp.js
+++ findadaycare-fe/src/components/ListDaycareFromYelp.js
@@ -4,51 +4,50 @@ import { connect } from 'react-redux';
 const ListDaycareFromYelp = (props) => {
 
   const renderReviews = (daycare) => {
-        if (!daycare.reviews) {
-            return;
-        }
+    if (!daycare.reviews) {
+      return;
+    }
 
-        return(
-            <ul>
-                {
-                    daycare.reviews.map((review) => {
-                        return (
-                            <li key={daycare.id}>
-                                <p>{review.text}</p>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        )
+    return (
+      <ul>
+        {
+          daycare.reviews.map((review) => {
+            return (
+              <li key={daycare.id}>
+                 <p>{review.text}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
+      )
     }
 
     const getReviews = (event, daycare) =>  {   
-        event.preventDefault();
-
+      event.preventDefault();
     }
 
     const renderDaycares = () => {
-        return props.daycares.map((daycare) => {
-            return (                               
-                <li key={daycare.id}>
-                    <div>
-                        <h2>{daycare.name}</h2>
-                        <h3>Rating: {daycare.rating}</h3>  
-                        <p>{daycare.location.display_address}</p>
-                        <button type="button" name="getReviews" onClick={(event) => getReviews(event, daycare)}>Read reviews</button>                    
-                    </div>    
-                </li>             
-            );
-        })
+      return props.daycares.map((daycare) => {
+        return (                               
+          <li key={daycare.id}>
+            <div>
+              <h2>{daycare.name}</h2>
+                <h3>Rating: {daycare.rating}</h3>  
+                  <p>{daycare.location.display_address}</p>
+                    <button type="button" name="getReviews" onClick={(event) => getReviews(event, daycare)}>Read reviews</button>                    
+            </div>    
+          </li>             
+        );
+      })
     }
 
     return (  
-        <div>  
-            <ul>
-                {renderDaycares()}
-            </ul>
-        </div> 
+      <div>  
+        <ul>
+          {renderDaycares()}
+        </ul>
+      </div> 
     )
 }
 
