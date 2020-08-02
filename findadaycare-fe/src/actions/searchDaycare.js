4b2a24c2ed74672bff4bdc338c52642c8481f568
Pignataro67 <Pignataro67@gmail.com>
2020-08-01 20:09:50 -0700

    adding action

--- findadaycare-fe/src/actions/searchDaycare.js
+++ findadaycare-fe/src/actions/searchDaycare.js
@@ -26,3 +26,24 @@ export const fetchDaycareFromYelp = (value) => {
       .then(res => dispatch(daycaresFromYelp(res)))
   }
 }
+
+export const loadingReviewsYelp = (business) => {
+  const businessId = business.id
+  const proxyurl = "https://cors-anywhere.herokuapp.com/"
+  const url = `https://api.yelp.com/v3/businesses/${businessId}/reviews`
+  return (
+      fetch(proxyurl + url, {
+          method: 'GET',
+          headers: {
+              'Accept': 'application/json',
+              'Authorization': 'Bearer 5BCE_9Zk-nhS7EcP1fYA0WcOf1L1Xq8hAY_mN5blb3Q4fH4Q-OqhBDxHGbEJstY8hOPn3WSyyHL_pdiaXiisk1fICFG445i4fUARZmQxiuhSudJo4YZ0RKOXV69SXXYx',
+              'Content-Type': 'application/json'
+          }
+      }).then(res => res.json())
+      .then(data => {
+          business.reviews =  data.reviews
+          return business
+      })
+  )
+  
+}
