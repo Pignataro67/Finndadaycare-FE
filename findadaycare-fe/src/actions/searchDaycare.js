d7be9d2bc372baf9e069a9c39d117ed89eac1690
Pignataro67 <Pignataro67@gmail.com>
2020-08-01 20:11:36 -0700

    spacing

--- findadaycare-fe/src/actions/searchDaycare.js
+++ findadaycare-fe/src/actions/searchDaycare.js
@@ -6,22 +6,21 @@ export const fetchDaycareFromYelp = (value) => {
   const proxyurl = "https://cors-anywhere.herokuapp.com/"
   const url = `https://api.yelp.com/v3/businesses/search?term=daycare&location=${value}&limit=5`
   return dispatch => {
-      fetch(proxyurl + url, {
-          method: 'GET',
-          headers: {
-              'Accept': 'application/json',
-              'Authorization': 'Bearer 5BCE_9Zk-nhS7EcP1fYA0WcOf1L1Xq8hAY_mN5blb3Q4fH4Q-OqhBDxHGbEJstY8hOPn3WSyyHL_pdiaXiisk1fICFG445i4fUARZmQxiuhSudJo4YZ0RKOXV69SXXYx',
-              'Content-Type': 'application/json'
-          }
-      }).then(res => res.json())
+    fetch(proxyurl + url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer 5BCE_9Zk-nhS7EcP1fYA0WcOf1L1Xq8hAY_mN5blb3Q4fH4Q-OqhBDxHGbEJstY8hOPn3WSyyHL_pdiaXiisk1fICFG445i4fUARZmQxiuhSudJo4YZ0RKOXV69SXXYx',
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
       .then(data => {
-          const businesses = data.businesses;
-          businesses.sort(function (a,b){
-              return b.rating - a.rating;
-          });        
+        const businesses = data.businesses;
+        businesses.sort(function (a,b){
+          return b.rating - a.rating;
+        });        
           const newBussinessesArray = businesses.map(business => loadingReviewsYelp(business)) 
-            
-          return newBussinessesArray
+        return newBussinessesArray
       })
       .then(res => dispatch(daycaresFromYelp(res)))
   }
@@ -32,18 +31,17 @@ export const loadingReviewsYelp = (business) => {
   const proxyurl = "https://cors-anywhere.herokuapp.com/"
   const url = `https://api.yelp.com/v3/businesses/${businessId}/reviews`
   return (
-      fetch(proxyurl + url, {
-          method: 'GET',
-          headers: {
-              'Accept': 'application/json',
-              'Authorization': 'Bearer 5BCE_9Zk-nhS7EcP1fYA0WcOf1L1Xq8hAY_mN5blb3Q4fH4Q-OqhBDxHGbEJstY8hOPn3WSyyHL_pdiaXiisk1fICFG445i4fUARZmQxiuhSudJo4YZ0RKOXV69SXXYx',
-              'Content-Type': 'application/json'
-          }
-      }).then(res => res.json())
+    fetch(proxyurl + url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer 5BCE_9Zk-nhS7EcP1fYA0WcOf1L1Xq8hAY_mN5blb3Q4fH4Q-OqhBDxHGbEJstY8hOPn3WSyyHL_pdiaXiisk1fICFG445i4fUARZmQxiuhSudJo4YZ0RKOXV69SXXYx',
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
       .then(data => {
-          business.reviews =  data.reviews
-          return business
+        business.reviews =  data.reviews
+        return business
       })
   )
-  
 }
