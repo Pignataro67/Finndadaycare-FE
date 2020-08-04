29a1b68bc6de012e147988ac14a4fc0ebb095d3a
Pignataro67 <Pignataro67@gmail.com>
2020-08-04 05:07:38 -0700

    adding comp.

---
+++ findadaycare-fe/src/components/Reviews.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reviews = (props) => {
+  return props.reviews.map(review => {
+    return (
+      <ul>
+        <li>{review.text}</li>
+      </ul>
+    )
+  })
+}
+export default Reviews;
