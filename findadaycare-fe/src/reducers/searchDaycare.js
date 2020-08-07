018efe3075d629ec2c580f165af2bdac3bb1f8ef
Pignataro67 <Pignataro67@gmail.com>
2020-08-07 04:39:08 -0700

    reducer

---
+++ findadaycare-fe/src/reducers/searchDaycare.js
@@ -0,0 +1,14 @@
+const initialState = { daycares: []};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "LIST_DAYCARES_SEARCHED":
+      state = {...state, daycares: state.daycares.concat(action.payload)}
+        state.daycares.sort(function (a,b){
+      return b.rating - a.rating;
+    });   
+      return state
+    default:
+      return state
+  }
+}
