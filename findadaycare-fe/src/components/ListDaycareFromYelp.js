8365124261e5f38e2c1777f0ed8c34c2a3382dc1
Pignataro67 <Pignataro67@gmail.com>
2020-08-08 06:19:35 -0700

    adding button, exporting fetchAddDaycare

--- findadaycare-fe/src/components/ListDaycareFromYelp.js
+++ findadaycare-fe/src/components/ListDaycareFromYelp.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Reviews from './Reviews.js';
+import Icon from '@mdi/react';
+import { mdiMapMarker } from '@mdi/js';
 
 const ListDaycareFromYelp = (props) => {
+
+  const handleOnClick = (e, daycare) => {
+    e.preventDefault();
+    console.log("Inside handleOnClick")
+    props.fetchAddDaycare(daycare)
+  }
 
   const renderDaycares = () => {
     return props.daycares.map((daycare) => {
       return (
-        <div className="daycare-card" key={daycare.id}>
+        <li className="daycare-card" key={daycare.id}>
           <img className="daycare__img" src={daycare.image_url} alt={`daycare: ${daycare.name}`}/>
           <h1>{daycare.name}</h1>
           <h2>Rating: {daycare.rating}</h2>  
           <p>{daycare.location.display_address} | {daycare.display_phone} </p>
-          <Reviews key={daycare.id} reviews={daycare.reviews}/>
-        </div>
+          <Reviews reviews={daycare.reviews}/>
+          <button onClick={(e) => handleOnClick(e, daycare)}>Add Daycare to my list</button>
+        </li>
       );
     })
   }
@@ -34,4 +43,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(ListDaycareFromYelp);
+export default connect(mapStateToProps, {fetchAddDaycare})(ListDaycareFromYelp);
