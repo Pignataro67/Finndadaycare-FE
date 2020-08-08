46f7020185ac9e4ca76c39d069c557aad5a91796
Pignataro67 <Pignataro67@gmail.com>
2020-08-08 06:26:41 -0700

    adding to backend

--- findadaycare-fe/src/actions/myDaycares.js
+++ findadaycare-fe/src/actions/myDaycares.js
@@ -32,3 +32,35 @@ export const fetchGetDaycare = () => {
   .catch(console.log)
   }
 } 
+
+// adding daycare to the backend
+export const fetchAddDaycare = (bussiness) => {
+  const daycare = {
+    name: bussiness.name,
+      location: bussiness.location.display_address.toString(),
+      rating: bussiness.rating,
+      review_count: bussiness.review_count,
+      yelp_id: bussiness.id,
+      phone: bussiness.display_phone
+  }
+  return dispatch => {
+    const url = 'http://localhost:3001/api/v1/daycares'
+    return fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(daycare)
+      }).then(res => res.json())
+        .then(data => {
+      if (data.error){
+        alert(data.error)
+      } else {
+    dispatch(addDaycare(data))
+      alert('Successfully bookmarked');
+    }
+  })
+  .catch(console.log)
+  }
+}
