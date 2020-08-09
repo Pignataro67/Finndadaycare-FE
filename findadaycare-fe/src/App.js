fd3e08f5cea4f58e00e3651b4e149dce46ec8a25
Pignataro67 <Pignataro67@gmail.com>
2020-08-09 07:59:45 -0700

    adding router and routes

--- findadaycare-fe/src/App.js
+++ findadaycare-fe/src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import MainContainer from './components/MainContainer';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser.js';
+import NavBar from './components/NavBar.js';
+import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import MyDaycares from './components/MyDaycares.js';
+import SearchDaycare from './components/SearchDaycare.js';
+import Signup from './components/Signup.js';
+import Logout from './components/Logout';
 
 class App extends React.Component {
   
@@ -14,7 +21,15 @@ class App extends React.Component {
   render() {
     return (
       <div  className="container">
-        <MainContainer/>
+        <NavBar/>
+        <Switch>
+          <Route exact path='/search' component={SearchDaycare}/>
+          <Route exact path='/login' component={Login}/> 
+          <Route exact path='/logout' component={Logout}/>
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/my-daycares' component={MyDaycares} />
+        </Switch>
+      <MainContainer/>        
       </div>
     );
   }
