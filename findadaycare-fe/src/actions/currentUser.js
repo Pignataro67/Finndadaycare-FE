d1fc8b4cc4d9ec7786e7f0a08eefa2133e70b461
Pignataro67 <Pignataro67@gmail.com>
2020-08-10 04:47:42 -0700

    adding action

--- findadaycare-fe/src/actions/currentUser.js
+++ findadaycare-fe/src/actions/currentUser.js
@@ -1,7 +1,42 @@
+import { resetSignupForm } from "./signupForm.js";
+
 export const setCurrentUser = user => {
   return {
       type: "SET_CURRENT_USER",
     user
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
+  }
+}
+
+export const signup = (credentials, history) => {
+  return dispatch => {
+    const userInfo = {
+      user: credentials
+    }
+    return fetch("http://localhost:3001/api/v1/signup", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(setCurrentUser(response.data))
+          dispatch(resetSignupForm())
+          history.push('/')
+        }
+      })
+      .catch(console.log)
   }
 }
 
