efaa38ffeb386cba71548aea1ed2ce0970cc4497
Pignataro67 <Pignataro67@gmail.com>
2020-08-12 06:45:42 -0700

    adding comp.

---
+++ findadaycare-fe/src/components/Signup.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateSignupForm } from '../actions/signupForm.js';
+import { signup } from '../actions/currentUser.js';
+
+const Signup = ({ signupFormData, updateSignupForm, signup, history }) => {
+
+  const handleUserInfoInputChange = event => {
+    const { name, value } = event.target
+    const updatedFormInfo = {
+      ...signupFormData,
+      [name]: value
+    }
+    updateSignupForm(updatedFormInfo)
+  }
+
+  return (
+    <form>
+      <label name="username"> Username: 
+        <input placeholder="username" value={signupFormData.name} name="username" type="text" onChange={handleUserInfoInputChange} />
+      </label><br></br>
+      <label name="email"> Email: 
+        <input placeholder="email" value={signupFormData.email} name="email" type="text" onChange={handleUserInfoInputChange} />
+      </label><br></br>
+      <label name="password"> Password: 
+        <input placeholder="password" value={signupFormData.password} name="password" type="password" onChange={handleUserInfoInputChange} />
+      </label><br></br>
+      <input type="submit" value="Sign Up"/>
+    </form>
+  )
+}
+
+export default Signup;
