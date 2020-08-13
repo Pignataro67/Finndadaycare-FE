edc4e1e043796ebfa912a14380f13f4c8cbe922f
Pignataro67 <Pignataro67@gmail.com>
2020-08-13 07:04:15 -0700

    adding route, imports, and minor debugging

--- findadaycare-fe/src/App.js
+++ findadaycare-fe/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MainContainer from './components/MainContainer';
+// import MainContainer from './components/MainContainer';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser.js';
 import NavBar from './components/NavBar.js';
 import { Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './components/Login.js';
 import MyDaycares from './components/MyDaycares.js';
 import SearchDaycare from './components/SearchDaycare.js';
 import Signup from './components/Signup.js';
-import Logout from './components/Logout';
+import Logout from './components/Logout.js';
+import Home from './components/Home.js';
 
 class App extends React.Component {
   
@@ -23,13 +24,13 @@ class App extends React.Component {
       <div  className="container">
         <NavBar/>
         <Switch>
-          <Route exact path='/search' component={SearchDaycare}/>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/search' component={SearchDaycare}/>   
           <Route exact path='/login' component={Login}/> 
           <Route exact path='/logout' component={Logout}/>
           <Route exact path='/signup' component={Signup} />
           <Route exact path='/my-daycares' component={MyDaycares} />
-        </Switch>
-      <MainContainer/>        
+        </Switch>      
       </div>
     );
   }
