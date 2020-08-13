f69a2b95a5988193d32e0f9172bb6e4636fc97b5
Pignataro67 <Pignataro67@gmail.com>
2020-08-13 06:45:22 -0700

    adding reducer case statement

--- findadaycare-fe/src/reducers/myDaycares.js
+++ findadaycare-fe/src/reducers/myDaycares.js
@@ -2,12 +2,12 @@ const initialState = { daycares: []};
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case "GET_DAYCARE":
+    case "GET_DAYCARES":
       state = {...state, daycares: action.payload}  
     return state
     case "ADD_DAYCARE":
       state = {...state, daycares: state.daycares.concat(action.payload)}  
-    return state
+    return state.daycares.data.attributes.my_daycares
       default:
     return state
   }
