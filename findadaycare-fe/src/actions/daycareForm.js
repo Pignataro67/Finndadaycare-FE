149883afdb43d8ee47516cf284960c03df4d7954
Pignataro67 <Pignataro67@gmail.com>
2020-08-15 06:52:05 -0700

    async fetch

--- findadaycare-fe/src/actions/daycareForm.js
+++ findadaycare-fe/src/actions/daycareForm.js
@@ -7,3 +7,31 @@ export const updateDaycareForm = (daycareForm) => {
       daycareForm
   }
 }
+
+export const fetchUpdateDaycare = (daycareFormData, userId, daycareId) => {
+  const sendDaycareData = {
+    notes: daycareFormData.notes,
+    schedule_visit: daycareFormData.scheduleVisit 
+  }
+  return dispatch => {
+    const url = `http://localhost:3001/api/v1/users/${userId}/my_daycares/${daycareId}`
+    return fetch(url, {
+      credentials: "include",
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+        body: JSON.stringify(sendDaycareData)
+    })
+      .then(r => r.json())
+      .then(res => {
+        if (res.error){
+          alert(res.error)
+        } 
+        else {
+          dispatch(updateDaycare(res))
+        }
+    })
+    .catch(console.log)
+  }
+}
