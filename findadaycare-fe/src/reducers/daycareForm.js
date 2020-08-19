f5e27d3e8c3e4d7b04bc410f0c00a673518f4b69
Pignataro67 <Pignataro67@gmail.com>
2020-08-19 07:22:17 -0700

    refrac. return, debugging

--- findadaycare-fe/src/reducers/daycareForm.js
+++ findadaycare-fe/src/reducers/daycareForm.js
@@ -6,8 +6,9 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_DAYCARE_FORM":
+    console.log("Inside reducer DaycareForm, my action is: ", action.daycareForm)
       state = {...state, [action.daycareForm.name]: action.daycareForm.value}
-      return state
+      return action.daycareForm //state
     default:
       return state
   }
