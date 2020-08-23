0cb32feedaf5af015e18fc4d8043921f93f39d14
Pignataro67 <Pignataro67@gmail.com>
2020-08-23 08:13:45 -0700

    adding use styles

--- findadaycare-fe/src/components/NavBar.js
+++ findadaycare-fe/src/components/NavBar.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Logout from './Logout.js';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
 
 const NavBar = ({ loggedIn }) => {
   return (
