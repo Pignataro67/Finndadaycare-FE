2fb0a7627e0d62ffc638f74de5750ddb7072b20d
Pignataro67 <Pignataro67@gmail.com>
2020-08-27 06:53:03 -0700

    stylin, improvements

--- findadaycare-fe/src/components/ListDaycareFromYelp.js
+++ findadaycare-fe/src/components/ListDaycareFromYelp.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Reviews from './Reviews.js';
 import Icon from '@mdi/react';
 import { mdiMapMarker } from '@mdi/js';
+import { fetchAddDaycare } from '../actions/myDaycares.js';
 import Button from "@material-ui/core/Button";
 import SaveIcon from '@material-ui/icons/Save';
 
@@ -21,9 +22,10 @@ const ListDaycareFromYelp = (props) => {
           <img className="daycare__img" src={daycare.image_url} alt={`daycare: ${daycare.name}`}/>
           <p className="daycare__address"> <Icon className="daycare__icon" path={mdiMapMarker} title="Daycare Address" description={daycare.location.display_address} size={1} color="red"/> {daycare.location.address1} {daycare.location.city}, {daycare.location.state} {daycare.location.zip_code}| {daycare.display_phone} </p>
           <h2 className="daycare__rating">Rating: {daycare.rating}</h2>
-          <p className="daycare__review"><strong>Reviews</strong></p>
+          <p className="daycare__review"><strong>Reviews</strong></p><p className="daycare__review"><strong>Reviews</strong></p>
           <Reviews reviews={daycare.reviews}/>
-          <Button onClick={(e) => handleOnClick(e, daycare)}variant="contained"
+          <Button onClick={(e) => handleOnClick(e, daycare)}
+            variant="contained"
             color="primary"
             size="large"
             className="daycare__button" 
