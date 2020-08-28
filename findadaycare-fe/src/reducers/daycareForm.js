869a7e57eabb06b1a39051c3ce29094be4952194
Pignataro67 <Pignataro67@gmail.com>
2020-08-28 07:26:40 -0700

    refractoring and strengthing reducer

--- findadaycare-fe/src/reducers/daycareForm.js
+++ findadaycare-fe/src/reducers/daycareForm.js
@@ -6,7 +6,6 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_DAYCARE_FORM":
-    console.log("Inside reducer DaycareForm, my action is: ", action.daycareForm)
       state = {...state, [action.daycareForm.name]: action.daycareForm.value}
       return  action.daycareForm
     case "RESET_DAYCARE_FORM":
--- findadaycare-fe/src/reducers/myDaycares.js
+++ findadaycare-fe/src/reducers/myDaycares.js
@@ -4,12 +4,12 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "GET_DAYCARES":
       state = {...state, daycares: action.payload}  
-        return state
+        return state.daycares.data.attributes.my_daycares
     case "ADD_DAYCARE":
       state = {...state, daycares: state.daycares.concat(action.payload)}  
-        return state.daycares.data.attributes.my_daycares
+        return state
     case "UPDATE_DAYCARE":
-        return state.map(daycare => daycare.id ===action.payload.id ? action.payload : daycare)    
+        return state.map(daycare => daycare.id === action.payload.id ? action.payload : daycare)    
     case "DELETE_DAYCARE":
         return state.filter(daycare => daycare.id === action.payload.id ? false : true)
       default:
